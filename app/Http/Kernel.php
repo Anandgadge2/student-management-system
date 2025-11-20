d5c09b34c4c7e0d6355e32003334b44c1b843616
Anandgadge2 <agadge797@gmail.com>
2025-11-20 18:33:26 +0530

    Add TrustProxies and force HTTPS for Render

---
+++ app/Http/Kernel.php
@@ -0,0 +1,7 @@
+protected $middleware = [
+    \App\Http\Middleware\TrustProxies::class,
+    \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
+    \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
+    \App\Http\Middleware\TrimStrings::class,
+    \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+];
